626451346e053b81c6b1a1137b80f9bb66e60cb5
SUPA93 <supaflavaz93@hotmail.com>
2024-02-28 01:16:27 +0100

    gitignore

--- lib/pdo.php
+++ lib/pdo.php
@@ -1,7 +1,7 @@
 <?php
-
-$pdo = new PDO('mysql:dbname=mfkrffbm_cuisinea;host=localhost;charset=utf8mb4', 'mfkrffbm_adminadmin', 'Bilallauis93350@');
-
-/* $pdo = new PDO('mysql:dbname=cuisinea;host=localhost;charset=utf8mb4', 'root', ''); */
+//ONLINE
+/* $pdo = new PDO('mysql:dbname=mfkrffbm_cuisinea;host=localhost;charset=utf8mb4', 'mfkrffbm_adminadmin', 'Bilallauis93350@'); */
+//LOCAL
+$pdo = new PDO('mysql:dbname=cuisinea;host=localhost;charset=utf8mb4', 'root', '');
 
 ?>
