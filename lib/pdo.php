742a497109ed1cae14c4e0e780f0d67f7fee4ff1
SUPA93 <supaflavaz93@hotmail.com>
2024-02-28 01:22:09 +0100

    removed pdo file

--- lib/pdo.php
+++
@@ -1,7 +0,0 @@
-<?php
-//ONLINE
-/* $pdo = new PDO('mysql:dbname=mfkrffbm_cuisinea;host=localhost;charset=utf8mb4', 'mfkrffbm_adminadmin', 'Bilallauis93350@'); */
-//LOCAL
-$pdo = new PDO('mysql:dbname=cuisinea;host=localhost;charset=utf8mb4', 'root', '');
-
-?>
